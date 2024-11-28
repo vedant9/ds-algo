efa57170024e3697326b0a99a638ec8524e75126
Vedant <vedant1999@gmail.com>
2024-11-28 18:02:14 +0530

    Array leetcode

---
+++ Arrays/MaxOnesInArray.cpp
@@ -0,0 +1,21 @@
+// https://leetcode.com/problems/max-consecutive-ones/description/
+
+class Solution {
+public:
+    int findMaxConsecutiveOnes(vector<int>& nums) {
+        int count = 0;
+        int max = 0;
+        for (int i=0; i< nums.size(); ++i) {
+            if(nums[i] == 1) {
+                ++count;
+                if(count>max){
+                    max = count;
+                }
+            }
+            else{
+                count = 0;
+            }
+        }
+        return max;
+    }
+};
