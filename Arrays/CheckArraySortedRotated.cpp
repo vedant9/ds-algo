c2385361f7062ca5432a34f025bfa881d85a8172
Vedant <vedant1999@gmail.com>
2024-11-28 17:04:13 +0530

    Array leetcode

---
+++ Arrays/CheckArraySortedRotated.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool check(vector<int>& nums) {
+        bool isArrayOk = true;
+        for (int i = 0; i < nums.size() - 1; ++i) {
+            if (nums[i] > nums[i + 1]) {
+                if (nums[i + 1] > nums[0]) {
+                    isArrayOk = false;
+                    break;
+                }
+                for (int j = i + 1; j < nums.size() - 1; ++j) {
+                    if (nums[j] > nums[j + 1] || nums[j] > nums[0] || nums[j + 1] > nums[0]) {
+                        isArrayOk = false;
+                        break;
+                    }
+                }
+            }
+        }
+        return isArrayOk;
+    }
+};
