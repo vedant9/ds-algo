7932c54097083049101a5e1beefd07011596a742
Vedant <vedant1999@gmail.com>
2025-01-12 18:21:53 +0530

    Array leetcode

--- Arrays/MajorityElement.cpp
+++ Arrays/MajorityElement.cpp
@@ -11,3 +11,38 @@ public:
         return -1;
     }
 };
+
+// Moore's voting algo (only main method)
+
+int majorityElement(const vector<int>& arr) {
+    int n = arr.size();
+    int candidate = -1;
+    int count = 0;
+
+    // Find a candidate
+    for (int num : arr) {
+        if (count == 0) {
+            candidate = num;
+            count = 1;
+        } else if (num == candidate) {
+            count++;
+        } else {
+            count--;
+        }
+    }
+
+    // Validate the candidate
+    count = 0;
+    for (int num : arr) {
+        if (num == candidate) {
+            count++;
+        }
+    }
+
+    // If count is greater than n / 2, return the candidate; otherwise, return -1
+    if (count > n / 2) {
+        return candidate;
+    } else {
+        return -1;
+    }
+}
