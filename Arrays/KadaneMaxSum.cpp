00cbfb6277fa81b02021a36cdc708eae34604b1c
Vedant <vedant1999@gmail.com>
2025-01-12 19:33:12 +0530

    Array leetcode

---
+++ Arrays/KadaneMaxSum.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int currSum = 0;
+        int maxSum = -99999;
+        for (int i=0; i<nums.size(); i++) {
+           currSum+=nums[i];
+           if(currSum>maxSum){
+            maxSum = currSum;
+           }
+           if(currSum<0){
+            currSum = 0;
+           }
+        }
+        return maxSum;
+    }
+};
